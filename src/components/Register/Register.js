7ffc8dbd061d354056be43a5e4599080ea07e2bd
ulyssetsd <utassidis@gmail.com>
2020-04-05 17:03:31 +0100

    #3 use form onSumit event instead of the click

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -25,7 +25,7 @@ class Register extends React.Component{
 		this.setState({registerName: event.target.value})
 	}
 
-	onSubmitRegister = () => {
+	onSubmitRegister = (event) => {
 		console.log(this.state);
 		fetch(process.env.REACT_APP_API_URL+'/register', {
 			method: 'post',
@@ -45,57 +45,59 @@ class Register extends React.Component{
 				this.props.onRouteChange('home');
 			})
 			.catch(error => this.setState({validationError: error.toString()}));
+		event.preventDefault();
 	}
 
 	render() {
 		return (
 			<main className="ma4 black-80 center">
 				<div className="measure center shadow-2 mw6 pa4 pl5 pr5 ba b--black br2">
-					<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-						<legend className="f2 fw6 ph0 mh0">Register</legend>
-						<div className="mt3">
-							<label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-							<input 
-								onChange={this.onNameChange}
-								className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 br2" 
-								type="text" 
-								name="name"
-								id="name" 
-								/>
-						</div>
-						<div className="mt3">
-							<label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-							<input 
-								onChange={this.onEmailChange}
-								className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 br2" 
-								type="email" 
-								name="email-address"
-								id="email-address" 
-								/>
-						</div>
-						<div className="mv3">
-							<label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-							<input 
-								onChange={this.onPasswordChange}
-								className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 br2" 
-								type="password" 
-								name="password"
-								id="password" 
-								/>
-						</div>
-					</fieldset>
-					<input 
-						onClick={this.onSubmitRegister} 
-						className="b ph3 pv2 ba b--black bg-transparent br2 grow pointer dib" 
-						type="submit" 
-						value="Register" 
-						/>
+					<form onSubmit={this.onSubmitRegister}>
+						<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+							<legend className="f2 fw6 ph0 mh0">Register</legend>
+							<div className="mt3">
+								<label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+								<input 
+									onChange={this.onNameChange}
+									className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 br2" 
+									type="text" 
+									name="name"
+									id="name" 
+									/>
+							</div>
+							<div className="mt3">
+								<label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+								<input 
+									onChange={this.onEmailChange}
+									className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 br2" 
+									type="email" 
+									name="email-address"
+									id="email-address" 
+									/>
+							</div>
+							<div className="mv3">
+								<label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+								<input 
+									onChange={this.onPasswordChange}
+									className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 br2" 
+									type="password" 
+									name="password"
+									id="password" 
+									/>
+							</div>
+						</fieldset>
+						<input 
+							className="b ph3 pv2 ba b--black bg-transparent br2 grow pointer dib" 
+							type="submit" 
+							value="Register" 
+							/>
+					</form>
+					{ this.state.validationError !== undefined && <ErrorAlert errorMessage={this.state.validationError} />}
 					<div
 						onClick={() => this.props.onRouteChange('signin')}  
 						className="lh-copy mt3">
 						<a href="#0" className="f6 link dim black db">Sign In</a>
 					</div>
-					{ this.state.validationError !== undefined && <ErrorAlert errorMessage={this.state.validationError} />}
 				</div>
 			</main>
 		)
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -19,7 +19,7 @@ class SignIn extends React.Component {
 		this.setState({signInPassword: event.target.value})
 	}
 
-	onSubmitSignIn = () => {
+	onSubmitSignIn = (event) => {
 		console.log(this.state);
 		fetch(process.env.REACT_APP_API_URL+'/signin', {
 			method: 'post',
@@ -38,6 +38,7 @@ class SignIn extends React.Component {
 				this.props.onRouteChange('home');
 			})
 			.catch(error => this.setState({validationError: error.toString()}));
+		event.preventDefault();
 	}
 
 	render() {
@@ -45,41 +46,42 @@ class SignIn extends React.Component {
 		return (
 			<main className="ma4 black-80 center">
 				<div className="measure center shadow-2 mw6 pa4 pl5 pr5 ba b--black br2">
-					<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-						<legend className="f2 fw6 ph0 mh0">Sign In</legend>
-						<div className="mt3">
-							<label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-							<input 
-								onChange={this.onEmailChange}
-								className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 br2" 
-								type="email" 
-								name="email-address"  
-								id="email-address" 
-								/>
-						</div>
-						<div className="mv3">
-							<label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-							<input 
-								onChange={this.onPasswordChange}
-								className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 br2" 
-								type="password" 
-								name="password"
-								id="password" 
-								/>
-						</div>
-					</fieldset>
-					<input 
-						onClick={this.onSubmitSignIn} 
-						className="b ph3 pv2 ba b--black bg-transparent br2 grow pointer dib" 
-						type="submit" 
-						value="Sign in" 
-						/>
+					<form onSubmit={this.onSubmitSignIn}>
+						<fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+							<legend className="f2 fw6 ph0 mh0">Sign In</legend>
+							<div className="mt3">
+								<label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+								<input 
+									onChange={this.onEmailChange}
+									className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 br2" 
+									type="email" 
+									name="email-address"  
+									id="email-address" 
+									/>
+							</div>
+							<div className="mv3">
+								<label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+								<input 
+									onChange={this.onPasswordChange}
+									className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100 br2" 
+									type="password" 
+									name="password"
+									id="password" 
+									/>
+							</div>
+						</fieldset>
+						<input 
+							className="b ph3 pv2 ba b--black bg-transparent br2 grow pointer dib" 
+							type="submit" 
+							value="Sign in" 
+							/>
+					</form>
+					{ this.state.validationError !== undefined && <ErrorAlert errorMessage={this.state.validationError} />}
 					<div 
 						onClick={() => onRouteChange('register')} 
 						className="lh-copy mt3">
 						<a href="#0" className="f6 link dim black db">Register</a>
 					</div>
-					{ this.state.validationError !== undefined && <ErrorAlert errorMessage={this.state.validationError} />}
 				</div>
 			</main>
 		)
